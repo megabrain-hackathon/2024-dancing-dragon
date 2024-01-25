2447c8a50a533e997c84daa8f4f05af32a300769
puleugo <puleugo@gmail.com>
2024-01-25 21:52:31 +0900

    feat: 드래곤 춤추기

--- src/App.js
+++ src/App.js
@@ -5,24 +5,69 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Toothless from './dragons/toothless.gif';
 
 export default class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      mode: 1
+      mode: 1,
+      dragons: [
+        {
+          height: 100,
+          width: 100,
+          x: 100,
+          y: 100,
+          degree: 10,
+        },
+        {
+          height: 100,
+          width: 100,
+          x: 100,
+          y: 100,
+          degree: 20,
+        },
+        {
+          height: 100,
+          width: 100,
+          x: 100,
+          y: 100,
+          degree: 30,
+        },
+        {
+          height: 100,
+          width: 100,
+          x: 100,
+          y: 100,
+          degree: 40,
+        },
+      ],
     }
   }
   
   render() {
     const editPanel = this.getEditPanel(this.state.mode);
     return (
-      <Container className="App">
-        <Row>
-          <Col style={{ height:'600px', backgroundColor: 'red'}}>
-            이미지 배경
+      <Container className='App' style={{width: '100%'}}>
+        <Row style={{width: '100%'}}>
+          <Col style={{ height:'600px', backgroundColor: 'white'}}>
+            {
+              this.state.dragons.map((dragon, index) => {
+                return (
+                  <img src={Toothless} alt={'toothless'} style={{
+                    height: dragon.height,
+                    width: dragon.width,
+                    transform: `rotate(${dragon.degree}deg)`,
+                    position: 'absolute',
+                    top: dragon.y,
+                    left: dragon.x,
+                    zIndex: index,
+                  }}/>
+                )
+              }, this)
+            }
           </Col>
-          <Col style={{ height:'600px', backgroundColor: 'yellow'}}>
+          <Col style={{height: '600px', backgroundColor: 'yellow'}}>
             <Button onClick={() => this.setState({mode: 1})}>버튼1</Button>
             <Button onClick={() => this.setState({mode: 2})}>버튼2</Button>
             <Button onClick={() => this.setState({mode: 3})}>버튼3</Button>
