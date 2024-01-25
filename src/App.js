6d006b14b3a09b721e89766c5da91d665a8c5229
puleugo <puleugo@gmail.com>
2024-01-25 23:20:58 +0900

    feat: background 관리 패널 틀 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 import Toothless from './dragons/toothless.gif';
 
 export default class App extends React.Component{
@@ -23,56 +25,100 @@ export default class App extends React.Component{
         {
           height: 100,
           width: 100,
-          x: 100,
-          y: 100,
+          x: 120,
+          y: 120,
           degree: 20,
         },
         {
           height: 100,
           width: 100,
-          x: 100,
-          y: 100,
+          x: 140,
+          y: 140,
           degree: 30,
         },
         {
           height: 100,
           width: 100,
-          x: 100,
+          x: 160,
+          y: 160,
+          degree: 40,
+        },
+
+        {
+          height: 100,
+          width: 100,
+          x: 600,
           y: 100,
           degree: 40,
         },
+        {
+          height: 100,
+          width: 100,
+          x: 580,
+          y: 120,
+          degree: 30,
+        },
+        {
+          height: 100,
+          width: 100,
+          x: 560,
+          y: 140,
+          degree: 20,
+        },
+        {
+          height: 100,
+          width: 100,
+          x: 540,
+          y: 160,
+          degree: 10,
+        },
       ],
+      background: {
+        width: 1000,
+        height: 1000,
+        color: 'white',
+        image: null,
+        mode: 1, // 1: color, 2: image
+      }
     }
   }
   
   render() {
+    const {dragons, background } = this.state;
     const editPanel = this.getEditPanel(this.state.mode);
     return (
       <Container className='App' style={{width: '100%'}}>
         <Row style={{width: '100%'}}>
           <Col style={{ height:'600px', backgroundColor: 'white'}}>
-            {
-              this.state.dragons.map((dragon, index) => {
-                return (
-                  <img src={Toothless} alt={'toothless'} style={{
-                    height: dragon.height,
-                    width: dragon.width,
-                    transform: `rotate(${dragon.degree}deg)`,
-                    position: 'absolute',
-                    top: dragon.y,
-                    left: dragon.x,
-                    zIndex: index,
-                  }}/>
-                )
-              }, this)
-            }
+            <div id='background' style={{
+              width: `${background.width}px`,
+              height: `${background.height}px`,
+              backgroundColor: background.mode === 1 ? background.color : null,
+              backgroundImage: background.mode === 2 ? background.image : null,
+              }}>
+              {
+                dragons.map((dragon, index) => {
+                  return (
+                    <img src={Toothless} alt={'toothless'} style={{
+                      height: dragon.height,
+                      width: dragon.width,
+                      transform: `rotate(${dragon.degree}deg)`,
+                      position: 'absolute',
+                      top: dragon.y,
+                      left: dragon.x,
+                      zIndex: index,
+                    }}/>
+                  )
+                }, this)
+              }
+            </div>
+            
           </Col>
           <Col style={{height: '600px', backgroundColor: 'yellow'}}>
-            <Button onClick={() => this.setState({mode: 1})}>버튼1</Button>
-            <Button onClick={() => this.setState({mode: 2})}>버튼2</Button>
-            <Button onClick={() => this.setState({mode: 3})}>버튼3</Button>
-            <Button onClick={() => this.setState({mode: 4})}>버튼4</Button>
-            <Button onClick={() => this.setState({mode: 5})}>버튼5</Button>
+            <Button onClick={() => this.setState({mode: 1})}>배경</Button>
+            <Button onClick={() => this.setState({mode: 2})}>드래곤</Button>
+            <Button onClick={() => this.setState({mode: 3})}>Music</Button>
+            <Button onClick={() => this.setState({mode: 4})}>저장</Button>
             {editPanel}
           </Col>
         </Row>
@@ -81,11 +127,26 @@ export default class App extends React.Component{
   }
   
   getEditPanel(id) {
+    
     switch (id) {
       case 1:
+        const {mode} = this.state.background;
+        // TODO: mode에 따라 색상 입력 혹은 이미지 입력
+        
         return (
           <div>
-            배경
+            <Form.Control size="lg" type="text" placeholder="가로" />
+            <Form.Control size="lg" type="text" placeholder="세로" />
+            <Form.Check inline name='background' type='radio'/>
+            <Form.Check inline name='background' type='radio'/>
+            <Form.Control
+              type="color"
+              id="exampleColorInput"
+              defaultValue="#FFFFFF"
+              title="Choose your color"
+              size="lg"
+              onChange={(e) => this.setState({background: {...this.state.background, color: e.target.value}})}
+            />
           </div>
         )
       case 2:
@@ -97,7 +158,7 @@ export default class App extends React.Component{
       case 3:
         return (
           <div>
-            BGM
+            Music
           </div>
         )
       case 4:
